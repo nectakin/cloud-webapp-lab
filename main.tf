e09dabdc050209fa8fb43cc5fca14738efe0561f
Кузьменко Вероніка Сергіївна <145161374+nectakin@users.noreply.github.com>
2026-03-23 22:51:41 +0200

    Remove AWS credentials from main.tf
    Removed sensitive AWS access and secret keys from configuration.

--- main.tf
+++ main.tf
@@ -1,7 +1,5 @@
 provider "aws" {
   region     = "eu-north-1"
-  access_key = "AKIAZIUV7DJLQE3DN2ZB"
-  secret_key = "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"
 }
 
 resource "aws_security_group" "web_sg" {
